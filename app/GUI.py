d79ec8535cd09576ad40367c120b0433038158a9
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2022-03-30 15:52:35 -0400

    Copy button

--- app/GUI.py
+++ app/GUI.py
@@ -1,5 +1,6 @@
 import PySimpleGUIQt as sg
 from converting import conversion
+import pyperclip as pc
 
 # link to different theme colors https://user-images.githubusercontent.com/46163555/70382042-796da500-1923-11ea-8432-80d08cd5f503.jpg
 sg.theme('DarkBlue2')
@@ -9,8 +10,9 @@ layout = [[sg.Text('Welcome to the Morse Code Translator!')],
               sg.InputText(key='-IN-')],
          # [sg.Text('Input morse code that you want translated to English. Please space out words with the | key.'),
           # sg.InputText(key='-INPUT-')],
-          [sg.Text(key='-OUTPUT-')],
-          [sg.Button('Translate'), sg.Button('Exit')]]
+          [sg.Text(key='-OUTPUT-')],[sg.Button('Copy')],
+          [sg.Button('Translate'), sg.Button('Exit')],
+          ]
 # Create the window
 window = sg.Window("Morse Code Translator", layout)
 
@@ -26,6 +28,9 @@ while True:
     # if user presses "translate"
     if event == 'Translate':
         window['-OUTPUT-'].update(y)
+
+    if event == 'Copy':
+        pc.copy(y)
     # End program if user closes window or
     # presses the Exit button
     if event == "Exit" or event == sg.WIN_CLOSED:
